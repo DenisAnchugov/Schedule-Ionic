e387e98e1f950d12775aca03f90b2140af0f5c9b
denis_anchugov <denis.anchugov@me.com>
2015-05-07 04:10:06 +0300

    Added subject room and teacher information. Visual improvements.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,5 +11,12 @@ angular.module('starter.controllers', [])
 .controller('SchedulePresentation', function($scope, $stateParams, ApiAccess) {
   ApiAccess.get('schedule/' + $stateParams.requestedSchedule).then(function(weekSchedule){
     $scope.weekSchedule = weekSchedule;
+    $scope.commas = function commas(items) {
+      var formattedRoomsString = [];
+      angular.forEach(items, function(value, key){
+      formattedRoomsString.push(value.name);
+      });
+      return formattedRoomsString.join(', ');
+    };
   });
 });
